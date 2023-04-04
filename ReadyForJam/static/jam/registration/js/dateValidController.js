8f1987da482fa0868f9c89d209aa1c89afc7736e
Денис <91241697+lanissell@users.noreply.github.com>
2023-04-04 20:03:38 +0700

    add: the data field is hidden if the previous value is not set

---
+++ ReadyForJam/static/jam/registration/js/dateValidController.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function (){
+    const dateStart = document.querySelector("#id_startDate");
+    const votingStartDate = document.querySelector("#id_votingStartDate");
+    const votingEndDate = document.querySelector("#id_votingEndDate");
+
+    const votingStartDateParent = votingStartDate.parentElement
+    votingStartDateParent.style.visibility = "hidden"
+
+    const votingEndDateParent = votingEndDate.parentElement
+    votingEndDateParent.style.visibility = "hidden"
+
+    dateStart.addEventListener("change", function (){
+        votingStartDateParent.style.visibility = "visible"
+        votingStartDate.setAttribute("min", dateStart.value)
+    })
+    votingStartDate.addEventListener("change", function (){
+        votingEndDateParent.style.visibility = "visible"
+        votingEndDate.setAttribute("min", dateStart.value)
+    })
+
+})
+
