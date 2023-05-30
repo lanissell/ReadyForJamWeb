1a34feef2e8422c98f31dfc09a7fdf19a8896692
Денис <91241697+lanissell@users.noreply.github.com>
2023-05-30 10:56:06 +0700

    cache in local

--- ReadyForJam/ReadyForJam/settings.py
+++ ReadyForJam/ReadyForJam/settings.py
@@ -124,8 +124,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': os.path.join(BASE_DIR, 'cache'),
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
     }
 }
 
